903a1a79c5a7ae585237928fffee270c03439d81
Federico Maleh <federico.maleh@wildlifestudios.com>
2022-05-07 13:51:31 -0300

    Clase del 2022-05-07

--- src/Library.hs
+++ src/Library.hs
@@ -1,7 +1,8 @@
 module Library where
 import PdePreludat
 
-palos = ["Corazones", "Picas", "Tréboles", "Diamantes"]
+palos = ["Corazones", "Picas", 
+         "Tréboles", "Diamantes"]
 
 type Carta = (Number, String)
 
@@ -20,3 +21,27 @@ mayorSegun f a b |f a > f b = a
 --1b
 maximoSegun _ [x] = x
 maximoSegun f (x:y:xs) = maximoSegun f (mayorSegun f x y :xs)
+
+maximoSegun' funcion lista = foldl1 (mayorSegun funcion) lista
+
+sinRepetidos [] = []
+sinRepetidos (x:xs) = x: (sinRepetidos.filter (/= x)) xs
+
+esCartaValida (numero, palo) = elem palo palos && elem numero [1..13]
+
+esoNoSeVale = not.esCartaValida
+
+manoNegra jugador = ((/=5).length.cartasEnMano) jugador || any esoNoSeVale (cartasEnMano jugador)
+
+ocurrenciasDe x = length . filter (== x)
+concatenar = foldl (++) []
+
+iguales n mano = any ((==n).flip ocurrenciasDe numeros) numeros
+        where numeros = map fst mano
+
+par = iguales 2
+pierna = iguales 3
+fullHouse mano = par mano && pierna mano
+
+otro:: Mano -> Bool
+otro _ = True
